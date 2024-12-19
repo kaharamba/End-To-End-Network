1353e1d337ed4434fe764f634a5cf683d1c88eb1
Conspiciens <fernandezjohann08@gmail.com>
2024-12-19 01:54:33 -0800

    Added validation step and input questions to easier handling

--- training.py
+++ training.py
@@ -13,14 +13,66 @@ from torch import nn
 from PIL import Image
 
 
-EPOCH = 100
+EPOCH = 1000
 transform = transforms.Compose([
     transforms.ToTensor()
 ]) 
 
 def eval_model(): 
-    model = torch.load("/Volumes/joeham/model.pth", weights_only=False) 
+    model = End_to_End_NN()
+    model.load_state_dict(torch.load("/Volumes/joeham/model.pth", weights_only=False)) 
     model.eval()  
+
+    # Validation data
+    image_folder = "/Volumes/joeham/valid_test_1/image_data/"
+    logging_folder = "/Volumes/joeham/valid_test_1/logging_data/"
+    merge_folder = "/Volumes/joeham/valid_test_1/"
+
+    data = preprocessing.data_processing(image_folder, logging_folder, merge_folder)
+    valid_dataloader = DataLoader(data, batch_size=1, shuffle=False)
+
+    epoch = 1
+    correct = 0
+    is_exit = None
+    with torch.no_grad():
+        for (front_img, steering) in valid_dataloader: 
+            output = model(front_img)
+            accuracy = is_correct(steering, output)
+
+            # Every 100 Images stop and anaylze the image and assoicated steering 
+            if epoch % 100 == 0 or is_exit == "x": 
+                is_exit = input("Press n for continue otherwise x to exit: ")
+
+                if is_exit == "x": 
+                    epoch += 1
+                    break
+                else: 
+                    print("Current steering checkpoint:", str(output))
+                    print("True steering checkpoint: ", str(steering))
+
+                    front_img = torch.squeeze(front_img)
+                    front_img = transforms.functional.to_pil_image(front_img)
+                    front_img.show()
+
+                    epoch += 1
+                    is_exit = input("Stopping, press any chracter to continue otherwise press \"x\" to exit : ")
+                    if is_exit == "x":
+                        return 
+
+                    continue 
+
+
+            if accuracy == 1.0:
+                correct += 1 
+
+
+            print("Current steering:", str(output))
+            print("True steering: ", str(steering))
+            print("Accuracy of the current Model: ", (correct / epoch))
+            epoch += 1 
+            
+    print("Total Accuracy of the current model: ", (correct / epoch))
+
 
 def is_correct(steering, output, threshold=0.1): 
     correct = torch.abs(steering - output) < threshold
@@ -30,7 +82,12 @@ def is_correct(steering, output, threshold=0.1):
 def train():
     save_model = input("Save model? (y or n) ") 
 
-    data = preprocessing.data_processing()
+    # Training data 
+    image_folder = "/Volumes/joeham/logging_camera_down/image_data/" 
+    logging_folder = "/Volumes/joeham/logging_camera_down/logging_data/"
+    merge_folder = "/Volumes/joeham/logging_camera_down/"
+
+    data = preprocessing.data_processing(image_folder, logging_folder, merge_folder)
     train_dataloader = DataLoader(data, batch_size=1, shuffle=True)
 
 	# Load the Model
@@ -78,7 +135,13 @@ def train():
 
 
 def main(): 
-    train()
+    is_train_or_valid = int(input("Would you like to train a model (1) or validate? (2) ")) 
+
+    if is_train_or_valid == 1: 
+        train()
+        return 
+    
+    eval_model()
 
 if __name__ == '__main__': 
     main()
